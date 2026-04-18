7d07fabb5e3f6c5b5e228b5d016d6eca3298ec55
Jack Martin <jrm9229@gmail.com>
2026-04-17 21:58:59 -0500

    setup navbar

---
+++ frontend/src/lib/components/NavBar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index';
+	import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
+	import { IsMobile } from '$lib/hooks/is-mobile.svelte';
+	import { resolve } from '$app/paths';
+
+	const isMobile = new IsMobile();
+</script>
+
+<NavigationMenu.Root viewport={isMobile.current}>
+	<NavigationMenu.List class="flex-wrap">
+		<NavigationMenu.Item>
+			<NavigationMenu.Link>
+				{#snippet child()}
+					<a href={resolve('/')} class={navigationMenuTriggerStyle()}>Home</a>
+				{/snippet}
+			</NavigationMenu.Link>
+		</NavigationMenu.Item>
+	</NavigationMenu.List>
+</NavigationMenu.Root>
