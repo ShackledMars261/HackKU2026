e3925784aaf3287b14faa4e1e13a417aa6126275
Jack Martin <jrm9229@gmail.com>
2026-04-17 22:44:54 -0500

    create get location page

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -3,3 +3,19 @@ export type User = {
   email: string;
   password: string;
 };
+
+export type Location = {
+    id: string;
+    latitude: number;
+    longitude: number;
+    name: string;
+    overallRating: number;
+}
+
+export type Post = {
+    userId: string;
+    locationId: string;
+    rating: number;
+    description: string;
+    photoFileIds: string[];
+}
