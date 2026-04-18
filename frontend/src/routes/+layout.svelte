13f715930606c11d07c8e02264680678c95d91b8
Jack Martin <jrm9229@gmail.com>
2026-04-18 16:06:32 -0500

    improve the navbar

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import NavBar from '@/components/NavBar.svelte';
-	let { children } = $props();
+
+	let { data, children } = $props();
 </script>
 
 <main class="relative flex h-screen flex-col overflow-hidden">
-	<div class="shrink px-2 py-2 bg-accent/50 border-b border-primary/20">
-		<NavBar />
+	<div class="w-full shrink border-b border-primary/20 bg-accent/50 px-2 py-2">
+		<NavBar isSignedIn={data.isSignedIn || false} />
 	</div>
 	<div class="relative flex grow overflow-hidden">
 		{@render children()}
