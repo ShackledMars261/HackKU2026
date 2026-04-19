5a6cc5ff0b36734012a480fa36bf1bbf2b1783c4
Jack Martin <jrm9229@gmail.com>
2026-04-19 03:10:50 -0500

    partially wire location page

--- frontend/src/routes/location/[slug]/+page.server.ts
+++ frontend/src/routes/location/[slug]/+page.server.ts
@@ -1,3 +1,4 @@
+import { getPostsByLocation } from '@/api/post/getPostsByLocation';
 import type { PageServerLoad } from './$types';
 import { getLocation } from '@/api/location/getLocation';
 
@@ -9,5 +10,8 @@ export const load: PageServerLoad = async ({ locals, params }) => {
 	const location = await getLocation(token, slug);
 	console.log(JSON.stringify(location));
 
-	return { location };
+	const posts = await getPostsByLocation(token, slug);
+	console.log(JSON.stringify(posts));
+
+	return { location, posts };
 };
